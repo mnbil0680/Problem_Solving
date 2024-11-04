9fc494380e09ff7dc3595c966e58d0759991bff8
Mohamed Nabil Mohamed <160195215+mnbil0680@users.noreply.github.com>
2024-11-04 04:20:38 +0200

    [3163] [Time Beats: 26.50%] [Memory Beats: 72.17%] - LeetPush

--- LeetCode/3163-String-Compression-III.cpp
+++ LeetCode/3163-String-Compression-III.cpp
@@ -17,4 +17,34 @@ public:
         }
         return comp;
     }
+    /*
+Another Solution runtime 0ms
+============================
+string compressedString(string word) {
+        string temp;
+        char last=word[0];
+        int cnt=1;
+        for(int i=1; i<word.size(); i++){
+            if(last==word[i]){
+                if(cnt<9){
+                    ++cnt;
+                    continue;
+                }else{
+                    temp.push_back(cnt+'0');
+                    temp.push_back(last);
+                    cnt=1;
+                }
+            }else{
+                temp.push_back(cnt+'0');
+                temp.push_back(last);
+                cnt=1;
+                last=word[i];
+            }
+        }
+        temp.push_back(cnt+'0');
+        temp.push_back(last);
+        return temp;
+
+*/
 };
+
