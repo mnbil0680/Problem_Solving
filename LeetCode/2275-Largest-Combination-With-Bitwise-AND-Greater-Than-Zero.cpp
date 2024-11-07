fd3793b6b2e2989835590cbdde7e2b65456be94e
Mohamed Nabil Mohamed <160195215+mnbil0680@users.noreply.github.com>
2024-11-07 16:51:07 +0200

    [2275] [Time Beats: 73.88%] [Memory Beats: 14.16%] - LeetPush

--- LeetCode/2275-Largest-Combination-With-Bitwise-AND-Greater-Than-Zero.cpp
+++ LeetCode/2275-Largest-Combination-With-Bitwise-AND-Greater-Than-Zero.cpp
@@ -1,3 +1,12 @@
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#pragma GCC optimize ("O3", "unroll-loops")
+#pragma GCC optimize ("-ffloat-store")
+
+auto init = []() { 
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    return 0;
+} ();
 class Solution {
 public:
     int largestCombination(vector<int>& candidates) {
